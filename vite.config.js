caf2a96160d76c69b63bc2c6a350575d931b5e78
Jessica Feliciano <jessfeliciano87@gmail.com>
2023-07-30 22:26:58 -0500

    updates index.html

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  // other config options...
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['vue-demi', '@vite/client', '@vite/env'],
-  },
   build: {
-    // outDir: './dist',
-    // base: '/SmartPrep/',
-    rollupOptions: {
-      output: {
-        // Ensures the MIME type is set to 'application/javascript'
-        mimeTypes: {
-          'js': 'application/javascript',
-        },
-      },
-    },
-    commonjsOptions: {
-      transformMixedEsModules: true,
-      // include: [/linked-dep/, /node_modules/],
-    },
+    outDir: 'dist',
   },
-})
+});
+
+
+// https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ['vue-demi', '@vite/client', '@vite/env'],
+//   },
+//   build: {
+//     // outDir: './dist',
+//     // base: '/SmartPrep/',
+//     rollupOptions: {
+//       output: {
+//         // Ensures the MIME type is set to 'application/javascript'
+//         mimeTypes: {
+//           'js': 'application/javascript',
+//         },
+//       },
+//     },
+//     commonjsOptions: {
+//       transformMixedEsModules: true,
+//       // include: [/linked-dep/, /node_modules/],
+//     },
+//   },
+// })
 
