48702bf53366f8b5ab6c0709da5c195a88ea4e93
Jessica Feliciano <jessfeliciano87@gmail.com>
2023-08-25 14:35:32 -0500

    makes groceries table mobile responsive

--- src/components/groceries/index.tsx
+++ src/components/groceries/index.tsx
@@ -27,46 +27,42 @@ const GroceriesPage = () => {
       <div className='bg-gray-100 py-8'>
         <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
           <PageHeader pageTitle={"Meal Planning"} />
-          <table className="min-w-full divide-y divide-gray-300 table-auto">
+
+          <table className="min-w-full divide-y divide-gray-300">
             <thead className="bg-gray-800">
               <tr>
-                <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
-                  <span className="sr-only">Purchased</span>
+                <th scope="col" className="py-3.5 !pl-4 pr-3 text-left text-sm font-semibold text-gray-100 sm:pl-0">
+                  Ingredient
                 </th>
-                <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-neutral-100 sm:pl-6">
+                <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-100 lg:table-cell">
                   Category
                 </th>
-                <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-neutral-100">
-                  Name
-                </th>
-                <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-neutral-100">
+                <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-100 sm:table-cell">
                   Amount
                 </th>
-                <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-neutral-100">
-                  Unit
+                <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-100 sm:pl-0">
+                  <span className="sr-only">Purchased</span>
+                  Purchased
                 </th>
               </tr>
             </thead>
             <tbody className="divide-y divide-gray-200 bg-white">
-            {ingredients && ingredients.map((ingredient) => (
-              <tr key={ingredient.id}>
-                <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                    <input type="checkbox" />
-                </td>
-                <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
+              {ingredients && ingredients.map((ingredient) => (
+                <tr key={ingredient.id}>
+                  <td className="w-full max-w-0 py-4 !pl-4 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:max-w-none sm:pl-0">
+                    {ingredient.name}
+                  </td>
+                  <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
                     {ingredient.category}
-                </td>
-                <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                  {ingredient.name}
-                </td>
-                <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                  {ingredient.amount}
-                </td>
-                <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                  {ingredient.unit}
-                </td>
-              </tr>
-            ))}
+                  </td>
+                  <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                    {ingredient.amount} {ingredient.unit}
+                  </td>
+                  <td className="relative whitespace-nowrap py-4 pl-3 pr-4 sm:pr-6">
+                      <input type="checkbox" />
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
